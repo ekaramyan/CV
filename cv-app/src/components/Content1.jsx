0c91a3aad0210b4ed6dab21974bb3b83c324cac5
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-01-07 16:31:04 +0400

    made a download button

--- cv-app/src/components/Content1.jsx
+++ cv-app/src/components/Content1.jsx
@@ -9,7 +9,9 @@ const Content1 = (props) => {
                     <p>a frontend developer</p>
                     <p>with passion to lear and create</p>
                 </div>
-                <button className='main__btn'>Download CV</button>
+                <a className='btn__container' href="https://docs.google.com/document/u/1/export?format=pdf&id=1ETQgt0fEBYm2fk6x1Pttcn-IeC0ujA7M&token=AC4w5Vjsdvg_Qo6a2oPIatlx6Pw0WQNKBA%3A1673093742596&ouid=100426792069851959092&includes_info_params=true&usp=drive_web&cros_files=false&inspectorResult=%7B%22pc%22%3A3%2C%22lplc%22%3A3%7D" download="CV">
+                    <button className='main__btn'>Download CV</button>
+                </a>
             </div>
         </main>
     )
