0104a354a77870c6c1acc064286d971d423abb2f
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-01-08 19:44:20 +0400

    fixing file system

--- cv-app/src/components/Content1.jsx
+++ cv-app/src/components/Content1.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 const Content1 = (props) => {
     return (
@@ -7,7 +7,7 @@ const Content1 = (props) => {
                 <div className='main__text'>
                     <p>Hi, my name is <span>{props.name}</span></p>
                     <p>a frontend developer</p>
-                    <p>with passion to lear and create</p>
+                    <p>with passion to learn and create</p>
                 </div>
                 <a className='btn__container' href="https://docs.google.com/document/u/1/export?format=pdf&id=1ETQgt0fEBYm2fk6x1Pttcn-IeC0ujA7M&token=AC4w5Vjsdvg_Qo6a2oPIatlx6Pw0WQNKBA%3A1673093742596&ouid=100426792069851959092&includes_info_params=true&usp=drive_web&cros_files=false&inspectorResult=%7B%22pc%22%3A3%2C%22lplc%22%3A3%7D" download="CV">
                     <button className='main__btn'>Download CV</button>
