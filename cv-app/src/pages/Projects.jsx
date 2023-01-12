9c43175189e77f49b9e34d97b54d8b6863206437
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-01-12 22:40:15 +0400

    remaking projects page

--- cv-app/src/pages/Projects.jsx
+++ cv-app/src/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import p1 from '../img/p1.png'
 
 
 export default function Projects(props) {
@@ -7,7 +8,7 @@ export default function Projects(props) {
             <div className="white-bg">
                 <h2>{props.title}</h2>
                 <div className="card">
-                    <iframe src="https://ekaramyan.github.io/Project-tofix/" frameborder="0" width={900} height={600}></iframe>
+                    <img src={p1} width={400}></img>
                     <p>{props.card_title}</p>
                 </div>
             </div>
