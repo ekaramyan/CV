3cc7a1336ca69a8680b442a303887b16aa41d905
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-01-20 01:04:27 +0400

    fixing styles, connecting API

--- cv-app/src/API/GitImport.jsx
+++ cv-app/src/API/GitImport.jsx
@@ -1,7 +1,28 @@
+import axios from 'axios'
 import React from 'react'
+import rest from 'rest'
+// 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
+async function GitImport() {
+ 
 
-export default function GitImport() {
-  return (
-    <div>GitImport</div>
-  )
+
+  axios.get('')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+  });
+
+  try {
+    const response = await axios.get('');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+
 }
+
+export default GitImport
