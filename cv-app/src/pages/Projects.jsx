9b105f32ce76f0f567dc307de8f830660dd8ad8b
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-01-20 21:18:30 +0400

    added animations and link to project for test

--- cv-app/src/pages/Projects.jsx
+++ cv-app/src/pages/Projects.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import p1 from '../img/p1.png'
-import { Link } from 'react-router-dom'
 
 export default function Projects(props) {
+    const project_link = 'https://ekaramyan.github.io/Project-tofix/'; //заглушка
     return (
         <>
             <div className="white-bg">
                 <h2>{props.title}</h2>
                 <div className="card">
-                    <img src={p1} width={400}></img>
-                    <p>{props.card_title}</p>
+                    <a href={project_link} target='_blank'>
+                        <img src={p1} width={400}></img>
+                        <p>{props.card_title}</p>
+                    </a>
                 </div>
+
             </div>
         </>
     )
