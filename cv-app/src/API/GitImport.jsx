8de81e143e682659283a27b519d6763a17e54dd1
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-01-27 23:45:22 +0400

    adding octokit

--- cv-app/src/API/GitImport.jsx
+++ cv-app/src/API/GitImport.jsx
@@ -1,20 +1,20 @@
 import axios from 'axios'
 import React from 'react'
 import rest from 'rest'
-// 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
+// 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 github token
 async function GitImport() {
- 
+
 
 
   axios.get('')
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .then(function () {
-  });
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {
+    });
 
   try {
     const response = await axios.get('');
---
+++ cv-app/src/API/Octokit.js
@@ -0,0 +1,5 @@
+import { Octokit } from "@octokit/rest"  
+export const octokit = new Octokit({     
+     auth: "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",    
+     userAgent: 'skylight v1' 
+});
