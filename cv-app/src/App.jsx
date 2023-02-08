6233dcdbd701a0a0ac774a7df2db7065afa52935
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-02-08 19:49:45 +0400

    added github api, fixing components

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './scss/styles.scss';
 import Header from './components/Header';
 import Content1 from './components/Content1';
 import Projects from './pages/Projects';
 
-function App(props) {
+function App() {
+
+
+  const [repos, setRepos] = useState([]);
+  const link = 'https://api.github.com/users/ekaramyan';
+  useEffect(() => {
+    const repo_get = async () => {
+      const result = await axios.get(link+'/repos'); //calling api
+      setRepos(result.data); //setting fetched data
+    }
+    repo_get() //calling function
+      .catch(console.error) //catching errors
+  }, []);
+
+  console.log(repos)
+
+
 
   return (
     <div className="content">
--- cv-app/src/pages/Projects.jsx
+++ cv-app/src/pages/Projects.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import p1 from '../img/p1.png'
 
-export default function Projects(props) {
-    const project_link = 'https://ekaramyan.github.io/Project-tofix/'; //заглушка
+export default function Projects(repos) {
+
+    const project_link = {repos}; //заглушка
     return (
         <>
             <div className="white-bg">
-                <h2>{props.title}</h2>
-                <div className="card">
-                    <a href={project_link} target='_blank'>
-                        <img src={p1} width={400}></img>
-                        <p>{props.card_title}</p>
-                    </a>
-                </div>
+
+               { repos.map()
+               //<h2>{repos.title}</h2>
+                // <div className="card">
+                //     <a href={project_link} target='_blank'>
+                //         <img src={repos} width={400}></img>
+                //         <p>{repos.card_title}</p>
+                //     </a>
+                // </div>
+               
+                }
 
             </div>
         </>
