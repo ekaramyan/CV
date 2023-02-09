bbdd2b964a51969f3d03121721e1e5377176d45b
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-02-09 16:47:24 +0400

    stylizied projects page

--- cv-app/src/components/ProjectItem.jsx
+++ cv-app/src/components/ProjectItem.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 const ProjectItem = ({ repo }) => {
 
     // const project_link = { url }; //заглушка
-    console.log(repo)
+
 
     if (!repo) {
         return null
     }
-
+    console.log(repo.owner.avatar_url)
     return (
 
         <div className="repoItem">
             <h2>{repo.name}</h2>
             <div className="card">
                 <a href={repo.svn_url} target='_blank'>
-                    {/* <img src={repos} width={400}></img> */}
+                    <img src={repo.owner.avatar_url} width={400}></img>
                     <p>{repo.name}</p>
                 </a>
             </div>
